cc67e505f9b723b426c2d85a7788e5c5865a8a2f
jesus <jesuscano@yahoo.es>
2021-03-01 11:56:39 +0100

    mas algos casi acabando

--- pruebas-algoritmos/everythingBeTrue.js
+++ pruebas-algoritmos/everythingBeTrue.js
@@ -9,10 +9,26 @@ In JavaScript, truthy values are values that translate to true when evaluated in
 Remember, you can access object properties through either dot notation or [] notation.
 */
 function truthCheck(collection, pre) {
-    return pre;
+   
+
+    for(let key of collection){
+        
+         if(Object.keys(key).includes(pre)==false){
+             
+             return false
+         }
+         
+         else if(Object.values(key).includes(0) || Object.values(key).includes("") ||
+          Object.values(key).includes(null) || Object.values(key).includes(undefined ) ||Object.values(key).includes(NaN)){
+             
+            return false
+         }
+                
+         
+
+    }
+    return true;
   }
   
-  truthCheck([{"user": "Tinky-Winky", "sex": "male"}, 
-  {"user": "Dipsy", "sex": "male"},
-   {"user": "Laa-Laa", "sex": "female"},
-    {"user": "Po", "sex": "female"}], "sex");
+
+  truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age");
